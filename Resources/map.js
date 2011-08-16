6366d4fed5c2424741e8cd5d4be0bae989cd98b7
Elena Trishina <e.trishina@hintsolutions.ru>
2011-08-16 07:57:30 +0400

    Карта с несколькими булавками

--- Resources/map.js
+++ Resources/map.js
@@ -26,8 +26,8 @@ var titleLabel = Titanium.UI.createLabel({
 	},
 	// shadowColor:'#151f12',
 	// shadowOffset: {
-		// x:1,
-		// y:1
+	// x:1,
+	// y:1
 	// },
 	text: 'Карта'
 });
@@ -77,10 +77,11 @@ navbar.add(blist);
 var mapview = Titanium.Map.createView({
 	mapType: Ti.Map.STANDART_TYPE,
 	region: {
-		latitude:55.7743911,
-		longitude:37.654634,
-		latitudeDelta:0.3,
-		longitudeDelta:0.3
+		//latitude:55.7743911,
+		latitude:55.6943911,
+		longitude:37.624634,
+		latitudeDelta:0.2,
+		longitudeDelta:0.2
 	},
 	animate:true,
 	regionFit:true,
@@ -98,3 +99,41 @@ var mapview = Titanium.Map.createView({
 });
 
 win.add(mapview);
+
+var mountainView = Ti.Map.createAnnotation({
+	latitude: 55.691337,
+	longitude: 37.620229,
+	title:"117105",
+	subtitle:'Варшавское шоссе д.18 к.2',
+	image:'images/pin1.png',
+	animate:true,
+	//rightButton:'images/arrow.png',
+	
+});
+mapview.addAnnotation(mountainView);
+var mountainView1 = Ti.Map.createAnnotation({
+	latitude: 55.690558,
+	longitude: 37.607145,
+	title:"117152",
+	subtitle:'Севастопольский пр., 11, к. 1',
+	image:'images/pin1.png',
+	animate:true,
+	//rightButton:'images/arrow.png',
+	
+});
+mapview.addAnnotation(mountainView1);
+var mountainView2 = Ti.Map.createAnnotation({
+	latitude: 55.689125,
+	longitude: 37.623410,
+	title:"117127",
+	subtitle:'Варшавское шоссе, 37',
+	image:'images/pin1.png',
+	animate:true,
+	//rightButton:'images/arrow.png',
+	
+});
+mapview.addAnnotation(mountainView2);
+mapview.zoom(1);
+mapview.zoom(1);
+mapview.zoom(1);
+mapview.zoom(1);
