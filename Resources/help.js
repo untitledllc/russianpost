1444cc9ca7bdf2c933bb97aa0a92ae34ce74e759
Elena Trishina <e.trishina@hintsolutions.ru>
2011-08-16 07:56:00 +0400

    Окно поиска индекса

--- Resources/help.js
+++ Resources/help.js
@@ -1,5 +1,5 @@
 var win = Ti.UI.currentWindow;
-win.backgroundImage = 'images/simple_fon.png';
+win.backgroundImage = 'images/main_fon.jpg';
 
 var navbar = Ti.UI.createView({
 	width: '100%',
@@ -29,9 +29,74 @@ var titleLabel = Titanium.UI.createLabel({
 		// x:1,
 		// y:1
 	// },
-	text: 'Индекс'
+	text: 'Поиск индекса'
 });
 
 // associate label to title
 viewTitle.add(titleLabel);
 navbar.add(viewTitle);
+
+
+var nearbut = Titanium.UI.createButton({
+	borderRadius: 3,
+	top: 140,
+	height:40,
+	width: 261,
+	backgroundImage: 'images/nearest.png',
+	backgroundSelectedImage: 'images/nearest_click.png'
+});
+
+
+nearbut.addEventListener('click', function(e) {
+	var winnear = Titanium.UI.createWindow(win.o.content);
+	winnear.url = 'nearest.js';
+	winnear.o = win.o;
+	winnear.open();
+});
+
+win.add(nearbut);
+
+//----------------------------------------
+var zmei = Ti.UI.createView({
+	height: 76,
+	width: 300,
+	top: 42,
+	backgroundImage: 'images/zmeika.png'
+});
+
+win.add(zmei);
+
+
+var labelzmei = Ti.UI.createLabel({
+	color:'#0c224d',
+	height: 'auto',
+	width:'auto',
+	top: 43,
+	textAlign:'center',
+	font: {
+		fontSize:20
+	},
+	text: 'Выберите тип поиска:'
+});
+
+zmei.add(labelzmei);
+
+var addressbut = Titanium.UI.createButton({
+	borderRadius: 3,
+	top: 188,
+	height:40,
+	//width: '88%',
+	width: 261,
+	backgroundImage: 'images/addresss.png',
+	backgroundSelectedImage: 'images/addresss_click.png'
+});
+
+
+addressbut.addEventListener('click', function(e) {
+	var winadrs = Titanium.UI.createWindow(win.o.content);
+	winadrs.url = 'adrsindex.js';
+	winadrs.o = win.o;
+	winadrs.open();
+});
+
+win.add(addressbut);
